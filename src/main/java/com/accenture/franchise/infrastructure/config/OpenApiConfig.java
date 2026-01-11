27c8842b35d71b34f4bf63039d5c40e004f196ec
Johs Salinas <johssalinas2work@gmail.com>
2026-01-11 12:39:54 -0500

    fix(OpenApiConfig): Corregir formato de la URL del servidor de producción en la configuración de OpenAPI

--- src/main/java/com/accenture/franchise/infrastructure/config/OpenApiConfig.java
+++ src/main/java/com/accenture/franchise/infrastructure/config/OpenApiConfig.java
@@ -32,7 +32,9 @@ public class OpenApiConfig {
         .servers(
             List.of(
                 new Server().url("http://localhost:8080").description("Servidor de Desarrollo"),
-                new Server().url("http://franchise-api-alb-2042942561.us-east-2.elb.amazonaws.com/api/v1' || 'http://localhost:8080").description("Servidor de Producción")
-            ));
+                new Server()
+                    .url(
+                        "http://franchise-api-alb-2042942561.us-east-2.elb.amazonaws.com/api/v1' || 'http://localhost:8080")
+                    .description("Servidor de Producción")));
   }
 }
